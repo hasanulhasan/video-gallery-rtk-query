55df611c413ae3c30b2b36a8e160b65d4f8d54ce
Md Hasanul Karim <hasanlink71@yahoo.com>
2023-03-15 23:12:34 +0600

    video fetched from server done

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,25 @@
+import { useGetVideosQuery } from "../../fetures/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
+    const { data: videos, isLoading, isError, error } = useGetVideosQuery();
+
+    //decide what to render
+    let content = null;
+    if (isLoading) content = <><VideoLoader /> <VideoLoader /><VideoLoader /><VideoLoader /></>;
+    if (!isLoading && isError) content = <Error message='there was an error' />;
+    if (!isLoading && !isError && videos?.length === 0) {
+        <Error message='there was no video' />;
+    }
+    if (!isLoading && !isError && videos?.length > 0) {
+        content = videos.map(video => <Video key={video.id} video={video} />)
+    }
+
     return (
         <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
+            {content}
         </>
     );
 }
